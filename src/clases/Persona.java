6eff30191464a79eb35c228b2a7b2aa60a270b11
Luis Tapia <tapia641@gmail.com>
2018-06-13 01:50:03 -0500

    se crea la clase Persona

---
+++ src/clases/Persona.java
@@ -0,0 +1,36 @@
+package clases;
+
+/**
+ * @author Luis Tapia
+ */
+public class Persona {
+    private String Nombre;
+    private short Edad;
+    private char Sexo;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public short getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(short Edad) {
+        this.Edad = Edad;
+    }
+
+    public char getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(char Sexo) {
+        this.Sexo = Sexo;
+    }   
+    
+    
+}
